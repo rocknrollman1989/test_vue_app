b886058518f1cb8ca2d2913af4c98751213300c4
Ilia Volkov <ilia.volkov@sagaris-soft.com>
2019-02-07 09:05:49 +0200

    add lib, init store

---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+const routes = [
+    // { path: '/foo', component: Foo },
+    // { path: '/bar', component: Bar }
+  ]
+export const router = new VueRouter({
+    routes
+})
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+
+export const store = new Vuex.Store({
+    state: {
+        blogPictures: [
+            {a: 1}
+        ]
+    }
+})
